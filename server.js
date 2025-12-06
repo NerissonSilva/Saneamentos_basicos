0a0bdd5c8dbb20ea4849f2d71bc1e444521d0b29
NerissonSilva <nerissonsilva486@gmail.com>
2025-12-05 21:23:24 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 const prisma = new PrismaClient();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5432;
 
 
 app.use(cors());
